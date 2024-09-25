9a618a5a73d41c7ea5aa28bda62d66e4e70acc64
Bruce <641138+abbott@users.noreply.github.com>
2024-09-25 04:32:20 -0400

    added DISABLE_OLLAMA

--- install.sh
+++ install.sh
@@ -14,23 +14,37 @@ read answer
 # Check if the user input is 'y' or 'Y'
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     # Initialize an empty string for build arguments
-	build_args=""
-	
-	# Adjust the path to the .env file
-	env_file=".env"
-	
-	# Check if the .env file exists
-	if [ ! -f "$env_file" ]; then
-	    echo "Error: .env file does not exist at $env_file"
-	    exit 1
-	fi
-	
-	# Read each line from .env, ignoring comments and empty lines
-	while IFS= read -r line; do
-	    if [[ ! $line =~ ^#.*$ ]] && [[ -n $line ]]; then
-	        build_args="$build_args --build-arg $line"
-	    fi
-	done < "$env_file"
+    build_args=""
+
+    # Adjust the path to the .env file
+    env_file=".env"
+
+    # Check if the .env file exists
+    if [ ! -f "$env_file" ]; then
+        echo "Error: .env file does not exist at $env_file"
+        exit 1
+    fi
+
+    # Init disable_ollama
+    disable_ollama=""
+
+    # Read each line from .env, ignoring comments and empty lines
+    while IFS= read -r line; do
+        # Skip comments and empty lines
+        if [[ ! $line =~ ^#.*$ ]] && [[ -n $line ]]; then
+            build_args="$build_args --build-arg $line"
+            
+            # Extract DISABLE_OLLAMA if present
+            if [[ $line =~ ^DISABLE_OLLAMA= ]]; then
+                disable_ollama="${line#*=}" # Extract the value after '='
+            fi
+        fi
+    done < "$env_file"
+
+    # Check if DISABLE_OLLAMA was found and process accordingly
+    if [ -n "$disable_ollama" ]; then
+        echo "DISABLE_OLLAMA is set to $disable_ollama"
+    fi
 
     # Navigate to the ts-web directory and build the Dockerfile
     echo "Building Docker image for ts-web..."
@@ -52,11 +66,15 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo "Starting services with docker compose..."
     docker compose up --detach
 
-#Uncomment to enable ts-gpu local ollama container running mistral
-   # Get the model installed on ts-gpt (requires curl)
-   # only if ollama is enabled in docker-compose.yaml
-    echo "Downloading  transcribe-ui mistral model"   
-    curl -X POST http://172.30.1.3:11434/api/pull -d '{"name": "transcribe-ui/transcribe-ui"}'
+    # Get the model installed on ts-gpt (requires curl)
+    # only if ollama is enabled in docker-compose.yaml
+    # Check if DISABLE_OLLAMA is set to "true"
+    if [ "$disable_ollama" != "true" ]; then
+        echo "Downloading transcribe-ui Mistral model"
+        curl -X POST http://172.30.1.3:11434/api/pull -d '{"name": "transcribe-ui/transcribe-ui"}'
+    else
+        echo "DISABLE_OLLAMA is true, skipping Mistral download."
+    fi
 
     # Re-attach to compose logs
     echo "Re-attaching to console logs"
